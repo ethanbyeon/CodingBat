f1d67722acf82655bf1e6c3d2a619cc3a2c082ff
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-06-17 22:21:57 -0700

    added warmup2 problems

---
+++ Warmup-2/last2.py
@@ -0,0 +1,16 @@
+
+def last2(str):
+  
+  if len(str) < 2:
+    return 0
+  
+  last = str[len(str) - 2:]
+  count = 0
+  
+  for i in range(len(str) - 2):
+    sub = str[i:i + 2]
+    if sub == last:
+      count += 1
+  
+  return count
+  
---
+++ Warmup-2/string_splosion.py
@@ -0,0 +1,10 @@
+
+def string_splosion(str):
+
+  result = ""
+
+  for i in range(len(str) + 1):
+    result += str[:i]
+    
+  return result
+  
